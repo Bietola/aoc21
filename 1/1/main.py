01cb96ec5a20537694703f4d96d25f7935f1faf3
Bietola <dincio.montesi@gmail.com>
2022-02-02 19:43:03 +0100

    Second dasher version

--- 1/1/main.py
+++ 1/1/main.py
@@ -1,15 +1,10 @@
-import fileinput
 from functools import reduce
+from fileinput import input
 from operator import sub
 
-inp = list(map(
-    int,
-    fileinput.input()
-))
+inp = list(map(int, input()))
 
-print(sum(
-    map(
-        lambda t: reduce(sub, t),
-        zip(inp[1:], inp)
-    )
-))
+print(sum(map(
+    lambda x: reduce(sub, x),
+    zip(inp[1:], inp)
+)))
